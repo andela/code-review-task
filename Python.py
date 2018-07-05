eddd558cc76a17e9ca43b63960d5f7ccc4b2b73c
IbrahimAndela <38470660+IbrahimAndela@users.noreply.github.com>
2018-07-05 16:07:57 +0100

    Create Python.py

---
+++ Python.py
@@ -0,0 +1,141 @@
+from rest_framework import generics, mixins, status, viewsets
+from rest_framework.exceptions import NotFound
+from rest_framework.permissions import (
+    AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly
+)
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Article, Comment, Tag
+from .renderers import ArticleJSONRenderer, CommentJSONRenderer
+from .serializers import ArticleSerializer, CommentSerializer, TagSerializer
+
+
+class ArticleViewSet(mixins.CreateModelMixin, 
+                     mixins.ListModelMixin,
+                     mixins.RetrieveModelMixin,
+                     viewsets.GenericViewSet):
+
+    lookup_field = 'slug'
+    queryset = Article.objects.select_related('author', 'author__user')
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    renderer_classes = (ArticleJSONRenderer,)
+    serializer_class = ArticleSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        author = self.request.query_params.get('author', None)
+        if author is not None:
+            queryset = queryset.filter(author__user__username=author)
+
+        tag = self.request.query_params.get('tag', None)
+        if tag is not None:
+            queryset = queryset.filter(tags__tag=tag)
+
+        favorited_by = self.request.query_params.get('favorited', None)
+        if favorited_by is not None:
+            queryset = queryset.filter(
+                favorited_by__user__username=favorited_by
+            )
+
+        return queryset
+
+    def create(self, request):
+        serializer_context = {
+            'author': request.user.profile,
+            'request': request
+        }
+        serializer_data = request.data.get('article', {})
+
+        serializer = self.serializer_class(
+        data=serializer_data, context=serializer_context
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def list(self, request):
+        serializer_context = {'request': request}
+        page = self.paginate_queryset(self.get_queryset())
+
+        serializer = self.serializer_class(
+            page,
+            context=serializer_context,
+            many=True
+        )
+
+        return self.get_paginated_response(serializer.data)
+
+    def retrieve(self, request, slug):
+        serializer_context = {'request': request}
+
+        try:
+            serializer_instance = self.queryset.get(slug=slug)
+        except Article.DoesNotExist:
+            raise NotFound('An article with this slug does not exist.')
+
+        serializer = self.serializer_class(
+            serializer_instance,
+            context=serializer_context
+        )
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+    def update(self, request, slug):
+        serializer_context = {'request': request}
+
+        try:
+            serializer_instance = self.queryset.get(slug=slug)
+        except Article.DoesNotExist:
+            raise NotFound('An article with this slug does not exist.')
+            
+        serializer_data = request.data.get('article', {})
+
+        serializer = self.serializer_class(
+            serializer_instance, 
+            context=serializer_context,
+            data=serializer_data, 
+            partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class CommentsListCreateAPIView(generics.ListCreateAPIView):
+    lookup_field = 'article__slug'
+    lookup_url_kwarg = 'article_slug'
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    queryset = Comment.objects.select_related(
+        'article', 'article__author', 'article__author__user',
+        'author', 'author__user'
+    )
+    renderer_classes = (CommentJSONRenderer,)
+    serializer_class = CommentSerializer
+
+    def filter_queryset(self, queryset):
+        # The built-in list function calls `filter_queryset`. Since we only
+        # want comments for a specific article, this is a good place to do
+        # that filtering.
+        filters = {self.lookup_field: self.kwargs[self.lookup_url_kwarg]}
+
+        return queryset.filter(**filters)
+
+    def create(self, request, article_slug=None):
+        data = request.data.get('comment', {})
+        context = {'author': request.user.profile}
+
+        try:
+            context['article'] = Article.objects.get(slug=article_slug)
+        except Article.DoesNotExist:
+            raise NotFound('An article with this slug does not exist.')
+
+        serializer = self.serializer_class(data=data, context=context)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
